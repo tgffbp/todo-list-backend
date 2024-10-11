293402d26da7c75801e605730098b575a67b9fed
Alena Gluzdikova <Lyoka@Grigoriis-MacBook-Pro.local>
2024-10-11 13:39:52 +0400

    first committ

---
+++ resources.py
@@ -0,0 +1,77 @@
+import os
+import json
+from typing import List
+from resources import Entry
+
+def print_with_indent(value, indent=0):
+    indentation = '\t' * indent
+    print(f'{indentation}{value}')
+
+
+class Entry:
+    def __init__(self, title, entries=None, parent=None):
+        self.title = title
+        if entries is None:
+            entries = []
+        self.entries = entries
+        self.parent = parent
+
+    def add_entry(self, entry):
+        self.entries.append(entry)
+        entry.parent = self
+
+    def print_entries(self, indent=0):
+        print_with_indent(self, indent)
+        for entry in self.entries:
+            entry.print_entries(indent=indent + 1)
+
+    def __str__(self):
+        return self.title
+
+    def json(self):
+        return {
+            "title": self.title,
+            "entries": [
+                entry.json() if isinstance(entry, Entry) else str(entry) for entry in self.entries
+            ]
+        }
+
+    @classmethod
+    def from_json(cls, value):
+        new_entry = cls(value['title'])
+        for sub_entry in value.get('entries', []):
+            new_entry.add_entry(cls.from_json(sub_entry))
+        return new_entry
+
+    def save(self, path):
+        file_path = f'{path}/{self.title}.json'
+        with open(file_path, 'w') as file:
+            json.dump(self.json(), file, indent=4)
+
+    @classmethod
+    def load(cls, filename):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        return cls.from_json(data)
+
+
+class EntryManager:
+    def __init__(self, data_path: str):
+        self.data_path: str = data_path
+        self.entries: List[Entry] = []
+
+    def save(self):
+        for entry in self.entries:
+            entry.save(self.data_path)
+
+    def load(self):
+        for filename in os.listdir(self.data_path):
+            if filename.endswith('.json'):
+                filepath = os.path.join(self.data_path, filename)
+                entry = Entry.load(filepath)
+                self.entries.append(entry)
+
+    def add_entry(self, title: str):
+        new_entry = Entry(title)
+        self.entries.append(new_entry)
+
---
+++ web-server.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from resources import EntryManager, Entry
+
+app = Flask(__name__)
+
+FOLDER = '/tmp/'
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/api/entries/")
+def get_entries():
+    entry_manager = EntryManager(FOLDER)
+    entry_manager.load()
+    entries_list = [entry.json() for entry in entry_manager.entries]
+    return entries_list
+
+
+@app.route("/api/save_entries/", methods=["POST"])
+def save_entries():
+    entry_manager = EntryManager(FOLDER)
+    data = request.get_json()
+
+    for item in data:
+        entry = Entry.from_json(item)
+        entry_manager.entries.append(entry)
+
+    entry_manager.save()
+    return {'status': 'success'}
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=False)
